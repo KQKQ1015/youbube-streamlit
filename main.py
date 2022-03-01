588494b43e196b35c590b635dc0edcaae6b28714
西原優空 <nishiharayura@nishiharayuukuus-MacBook-Air.local>
2022-03-01 22:55:13 +0900

    1st commit

---
+++ main.py
@@ -0,0 +1,30 @@
+from curses import A_ALTCHARSET
+from re import A
+import streamlit as st
+import numpy as np
+import pandas as pd
+from PIL import Image
+import time
+
+
+
+st.title('Streamlit 超入門')
+
+st.write('プログレスバーの表示\nSTART!!')
+
+latest_iteration = st.empty()
+bar = st.progress(0)
+
+for i in range(100):
+    latest_iteration.text(f'Iteration{i+1}')
+    bar.progress(i+1)
+    time.sleep(0.01)
+
+left_columu, right_columu = st.columns(2)
+
+button = left_columu.button('右カラムにボタンを追加')
+if button:
+    right_columu.write('ここは右カラム')
+
+expander = st.expander('お問合せ')
+expander.write('お問合せ内容をかく')
